9ff1bf9754a9ce01b2416cfe04b6e5a7c791f9c5
Ayyappan <131690195+Ayyappan-M-P@users.noreply.github.com>
2024-08-09 16:46:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,17 @@
 # # config.py
 
-# SMTP_SERVER = 'smtp.example.com'
-# SMTP_PORT = 587
-# SENDER_EMAIL = 'ayyappanmp.cse2022@citchennai.net'
-# SENDER_PASSWORD = '17/12/2004//'
-# TARGET_EMPLOYEES = [
-#     'mpayyappan2004@gmail.com',
-#     'ayyappanmp2026@gmail.com',
-#     # Add more employee emails here
-# ]
 
 
 
 # config.py
 
-SENDGRID_API_KEY = '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'
-SENDER_EMAIL = 'ayyappanmp.cse2022@citchennai.net'  # Your verified sender email
+SENDGRID_API_KEY = 'sendgrid api'
+SENDER_EMAIL = 'sender email'  # Your verified sender email
 
 # List of target employee emails
 TARGET_EMPLOYEES = [
-    'ayyappanmp2026@gmail.com',
-    'mpayyappan2004@gmail.com'
+    'target email 1',
+    'target email 2'
 ]
 
 
